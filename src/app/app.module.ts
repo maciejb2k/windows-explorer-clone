48f99150b5f90ed554c7fa21688c0415d817ebd4
Maciej Biel <mb117780@stud.ur.edu.pl>
2022-04-19 12:46:53 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WindowComponent } from './components/window/window.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { RibbonComponent } from './components/ribbon/ribbon.component';
+import { AddressBarComponent } from './components/address-bar/address-bar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { TreeViewComponent } from './components/tree-view/tree-view.component';
+import { FolderViewComponent } from './components/folder-view/folder-view.component';
+import { StatusBarComponent } from './components/status-bar/status-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent,
+    TitleBarComponent,
+    RibbonComponent,
+    AddressBarComponent,
+    SearchBoxComponent,
+    TreeViewComponent,
+    FolderViewComponent,
+    StatusBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/address-bar/address-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-bar',
+  templateUrl: './address-bar.component.html',
+  styleUrls: ['./address-bar.component.scss']
+})
+export class AddressBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
